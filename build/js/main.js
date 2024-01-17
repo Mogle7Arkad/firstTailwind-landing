d0625ab184fbf6a31acce12ef0edc90febb72921
Ali George <george7ali@gmail.com>
2024-01-17 14:44:11 +0100

    call the funtion

--- build/js/main.js
+++ build/js/main.js
@@ -6,7 +6,9 @@ function initApp() {
         dropDown.classList.toggle('hidden');
         dropDown.classList.toggle('flex');
     }
-
+    
     hamburger.addEventListener('click', toggleMenu)
     dropDown.addEventListener('click', toggleMenu)
 }
+
+document.addEventListener('DOMContentLoaded', initApp)
