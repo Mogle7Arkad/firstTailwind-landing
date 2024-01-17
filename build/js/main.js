1e8162c9a9605ce1856a0c28f2019898900285d3
Ali George <george7ali@gmail.com>
2024-01-17 14:39:11 +0100

    add event listener to the dowm elements to activate the toggle effect

--- build/js/main.js
+++ build/js/main.js
@@ -6,4 +6,7 @@ function initApp() {
         dropDown.classList.toggle('hidden');
         dropDown.classList.toggle('flex');
     }
+
+    hamburger.addEventListener('click', toggleMenu)
+    dropDown.addEventListener('click', toggleMenu)
 }
