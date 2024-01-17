889b993625ffc723c060a3a9bf8c9118f2cc565e
Ali George <george7ali@gmail.com>
2024-01-17 14:38:04 +0100

    create function togglemenu and add toggle classlist to the dom elements

--- build/js/main.js
+++ build/js/main.js
@@ -1,4 +1,9 @@
 function initApp() {
     const hamburger = document.getElementById('hamburger');
     const dropDown = document.getElementById('dropdown-menu');
+
+    function toggleMenu() {
+        dropDown.classList.toggle('hidden');
+        dropDown.classList.toggle('flex');
+    }
 }
